8a7dd4a722dc33da912d5baeb6e4d5c31f168d21
Shilei Yang <syan0156@student.monash.edu>
2026-03-14 04:12:29 +1100

    Pre-deployment checks for the backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,6 +10,8 @@ def create_app():
             "origins": [
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
+                "http://localhost:5174",
+                "http://127.0.0.1:5174",
                 "http://localhost:3000"
             ]
         }
