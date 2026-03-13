323ec842e749ca290be3310b5a80bfd633e88f80
Shilei Yang <syan0156@student.monash.edu>
2026-03-14 05:29:47 +1100

    allow vercel frontend in CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,7 +12,8 @@ def create_app():
                 "http://127.0.0.1:5173",
                 "http://localhost:5174",
                 "http://127.0.0.1:5174",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://sunsmart-platform.vercel.app"
             ]
         }
     })
